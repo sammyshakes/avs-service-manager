6d9a19613998618c1dd5218b483d6c107a4ce7ab
sammyshakes <sam.dev.web3@gmail.com>
2024-05-23 08:37:43 -0700

    generate operator function

--- script/RegisterOperator.s.sol
+++ script/RegisterOperator.s.sol
@@ -7,63 +7,76 @@ import {IBLSApkRegistry} from "@eigenlayer-middleware/src/interfaces/IBLSApkRegi
 import {BN254} from "@eigenlayer-middleware/src/libraries/BN254.sol";
 import {IDelegationManager} from "@eigenlayer/src/contracts/interfaces/IDelegationManager.sol";
 import {IStrategy} from "@eigenlayer/src/contracts/interfaces/IStrategy.sol";
-
-///////////////
-/// NOTE: This script currently only registers operators
-/// with the Delegation Manager
-///////////////
+import {IAVSDirectory} from "@eigenlayer/src/contracts/interfaces/IAVSDirectory.sol";
 
 contract RegisterOperatorScript is Script {
     RegistryCoordinator public registryCoordinator;
     IDelegationManager public delegationManager;
+    IAVSDirectory public avsDirectory;
 
     function run() external {
         uint256 _deployerPrivateKey = uint256(vm.envBytes32("DEPLOYER_PRIVATE_KEY"));
         address _registryCoordinator = vm.envAddress("REGISTRY_COORDINATOR");
         address _delegationManager = vm.envAddress("DELEGATION_MANAGER");
+        address _avsDirectory = vm.envAddress("AVS_DIRECTORY");
 
         registryCoordinator = RegistryCoordinator(_registryCoordinator);
         delegationManager = IDelegationManager(_delegationManager);
+        avsDirectory = IAVSDirectory(_avsDirectory);
 
-        // Configure operator details
         uint256 numberOfOperators = 5; // Change this to the number of operators you want to register
-        address[] memory operators = new address[](numberOfOperators);
-        uint256[] memory operatorPrivateKeys = new uint256[](numberOfOperators);
-        bytes32[] memory operatorIds = new bytes32[](numberOfOperators);
-        bytes[] memory quorumNumbersArray = new bytes[](numberOfOperators);
-        string memory socket = "127.0.0.1";
-        IBLSApkRegistry.PubkeyRegistrationParams memory params;
-
-        // Initialize mock BLS public key params
-        params.pubkeyRegistrationSignature = BN254.G1Point(0, 0);
-        params.pubkeyG1 = BN254.G1Point(0, 0);
-        params.pubkeyG2 = BN254.G2Point([uint256(0), uint256(0)], [uint256(0), uint256(0)]);
+        (uint256[] memory privateKeys, address[] memory operators, BN254.G1Point[] memory publicKeys) =
+            _generateOperators(_deployerPrivateKey, numberOfOperators);
 
         for (uint256 i = 0; i < numberOfOperators; i++) {
-            operatorPrivateKeys[i] = uint256(keccak256(abi.encodePacked(i, _deployerPrivateKey + 1)));
-            operators[i] = vm.addr(operatorPrivateKeys[i]);
-            operatorIds[i] = keccak256(abi.encodePacked(operators[i]));
-            quorumNumbersArray[i] = abi.encodePacked(uint8(i + 1));
-
-            // Register operator with Delegation Manager using the operator's private key
-            vm.startBroadcast(operatorPrivateKeys[i]);
+            vm.startBroadcast(privateKeys[i]);
             _registerWithDelegationManager(operators[i]);
-            // _registerOperator(quorumNumbersArray[i], operators[i], operatorPrivateKeys[i]);
+            require(delegationManager.isOperator(operators[i]), "Operator not registered with delegation manager");
+            _registerOperator(
+                abi.encodePacked(uint8(i + 1)), operators[i], privateKeys[i], publicKeys[i], _avsDirectory
+            );
             vm.stopBroadcast();
         }
 
-        // vm.startBroadcast(_deployerPrivateKey);
+        vm.startBroadcast(_deployerPrivateKey);
+        for (uint256 i = 0; i < numberOfOperators; i++) {
+            _registerOperator(
+                abi.encodePacked(uint8(i + 1)), operators[i], privateKeys[i], publicKeys[i], _avsDirectory
+            );
+        }
+        vm.stopBroadcast();
+    }
 
-        // // Register operators with RegistryCoordinator
-        // for (uint256 i = 0; i < numberOfOperators; i++) {
-        //     _registerOperator(quorumNumbersArray[i], operators[i], operatorPrivateKeys[i]);
-        // }
+    function _generateOperators(uint256 deployerPrivateKey, uint256 numberOfOperators)
+        internal
+        view
+        returns (uint256[] memory, address[] memory, BN254.G1Point[] memory)
+    {
+        uint256[] memory privateKeys = new uint256[](numberOfOperators);
+        address[] memory operators = new address[](numberOfOperators);
+        BN254.G1Point[] memory publicKeys = new BN254.G1Point[](numberOfOperators);
 
-        // vm.stopBroadcast();
+        for (uint256 i = 0; i < numberOfOperators; i++) {
+            privateKeys[i] = generateValidPrivateKey(i, deployerPrivateKey);
+            operators[i] = vm.addr(privateKeys[i]);
+            publicKeys[i] = generatePublicKey(privateKeys[i]);
+        }
+
+        return (privateKeys, operators, publicKeys);
+    }
+
+    function generateValidPrivateKey(uint256 index, uint256 deployerPrivateKey) internal pure returns (uint256) {
+        uint256 privateKey = uint256(keccak256(abi.encodePacked(index, deployerPrivateKey))) % BN254.FR_MODULUS;
+        require(privateKey > 0 && privateKey < BN254.FR_MODULUS, "Invalid private key");
+        return privateKey;
+    }
+
+    function generatePublicKey(uint256 privateKey) internal view returns (BN254.G1Point memory) {
+        BN254.G1Point memory g1 = BN254.generatorG1();
+        return BN254.scalar_mul(g1, privateKey);
     }
 
     function _registerWithDelegationManager(address operator) internal {
-        // Register operator to EigenLayer
         delegationManager.registerAsOperator(
             IDelegationManager.OperatorDetails({
                 earningsReceiver: operator,
@@ -74,24 +87,26 @@ contract RegisterOperatorScript is Script {
         );
     }
 
-    function _registerOperator(bytes memory quorumNumbers, address operator, uint256 operatorPrivateKey) internal {
-        // Get operator signature
-        ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature = _getOperatorSignature(
-            operator,
-            address(registryCoordinator),
-            bytes32(0), // emptySalt
-            type(uint256).max, // maxExpiry
-            operatorPrivateKey
-        );
+    function _registerOperator(
+        bytes memory quorumNumbers,
+        address operator,
+        uint256 operatorPrivateKey,
+        BN254.G1Point memory operatorPublicKey,
+        address avsDirectoryAddress
+    ) internal {
+        ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature =
+            _getOperatorSignature(operator, avsDirectoryAddress, bytes32(0), type(uint256).max, operatorPrivateKey);
 
-        // Register operator
+        // Generate realistic G2 public key
+        BN254.G2Point memory operatorPublicKeyG2 = BN254.generatorG2();
+
         registryCoordinator.registerOperator(
             quorumNumbers,
             "127.0.0.1",
             IBLSApkRegistry.PubkeyRegistrationParams({
                 pubkeyRegistrationSignature: BN254.G1Point(0, 0),
-                pubkeyG1: BN254.G1Point(0, 0),
-                pubkeyG2: BN254.G2Point([uint256(0), uint256(0)], [uint256(0), uint256(0)])
+                pubkeyG1: operatorPublicKey,
+                pubkeyG2: operatorPublicKeyG2
             }),
             operatorSignature
         );
@@ -99,40 +114,18 @@ contract RegisterOperatorScript is Script {
 
     function _getOperatorSignature(
         address operatorToSign,
-        address avs,
+        address avsDirectoryAddress,
         bytes32 salt,
         uint256 expiry,
         uint256 operatorPrivateKey
     ) internal view returns (ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature) {
-        operatorSignature.salt = salt;
-        operatorSignature.expiry = expiry;
+        bytes32 digestHash =
+            avsDirectory.calculateOperatorAVSRegistrationDigestHash(operatorToSign, avsDirectoryAddress, salt, expiry);
 
-        // Calculate the EIP-712 hash
-        bytes32 domainSeparator = keccak256(
-            abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes("EigenLayer")),
-                keccak256(bytes("1")),
-                block.chainid,
-                address(registryCoordinator)
-            )
-        );
-
-        bytes32 structHash = keccak256(
-            abi.encode(
-                keccak256("OperatorRegistration(address operator,address avs,bytes32 salt,uint256 expiry)"),
-                operatorToSign,
-                avs,
-                salt,
-                expiry
-            )
-        );
-
-        bytes32 digestHash = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-
-        // Sign the digest hash
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(operatorPrivateKey, digestHash);
         operatorSignature.signature = abi.encodePacked(r, s, v);
+        operatorSignature.salt = salt;
+        operatorSignature.expiry = expiry;
 
         return operatorSignature;
     }
